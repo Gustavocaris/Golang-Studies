061a22bf2dc4c86c7db5c356d3f7273a12dc0f82
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-11 15:55:07 -0300

    feat: add other examples of variables

--- 01-foundations/B-variables.go
+++ 01-foundations/B-variables.go
@@ -3,3 +3,22 @@
 // Primeira forma VAR: Usada quando você precisa declarar uma variável sem inicializá-la imediatamente (ela assume o Zero Value) ou no escopo de pacote (fora de funções).
 
 var age int // Declaração de variável do tipo int, sem inicialização (Zero Value é 0)
+
+var name string // Declaração de variável do tipo string, sem inicialização (Zero Value é "")
+
+var isActive bool // Declaração de variável do tipo bool, sem inicialização (Zero Value é false)
+
+
+
+// Segunda forma, seria a curta (:=) — O Padrão em Funções
+//Usada dentro de funções. O compilador infere o tipo com base no valor atribuído à direita.
+
+count := 10        // O compilador infere como int
+message := "Golang" // O compilador infere como string
+
+
+
+
+
+
+
