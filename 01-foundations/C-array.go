351f8fceeab87f218361b59d7cd9d2cf2addcfb6
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-12 07:53:41 -0300

    feat: add type array in go

---
+++ 01-foundations/C-array.go
@@ -0,0 +1,15 @@
+// Um array é uma coleção de elementos do mesmo tipo com tamanho fixo.
+//Os índices começam em 0
+
+Package main
+
+import "fmt"
+
+
+servers := [3]string{
+	"server-01",
+	"server-02",
+	"server-03",
+}
+
+fmt.Println(servers[0])
