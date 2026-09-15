2adff2aba22254dc66333f563abcfcaf16f52588
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-15 15:37:18 -0300

    feat(hands-on) initial structure in my function, not is correct. i try :)

---
+++ hands-on/01-list/exe-two.go
@@ -0,0 +1,11 @@
+// faça um programa que de bom dia, 
+// pergunte o nome da pessoa e reponde que é um prazer conhece-la sitadno o nome da pessoa.
+
+package main
+
+import "fmt"
+
+funct main() {
+	fmt.println("hello, what do you name?")
+	scan("")
+}
