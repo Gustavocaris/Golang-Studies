190c5a609afc17d9dd66393477016f3347191a0e
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-19 14:28:57 -0300

    feat(hands-on): start exercise number 3

---
+++ hands-on/01-list/03exe/main.go
@@ -0,0 +1,12 @@
+// faça um programinha que exiba o dobro de um número informado pelo usuário
+
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = x
+
+	fmt.Println("insira um número:")
+	fmt.Scanf("%f", &x)
+}
