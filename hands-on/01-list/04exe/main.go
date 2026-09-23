1b1cfe60a62189188cba6770840a8dc00c6cc4eb
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-23 18:44:58 -0300

    feat: Text on screen, starting test.

--- hands-on/01-list/04exe/main.go
+++ hands-on/01-list/04exe/main.go
@@ -1,1 +1,10 @@
+// faça um programa que vende potes de Whey.
+// Se o cliente escolher whey normal, o preço é 100 reais.
+// Se o cliente escolher whey isolado, o preço é 150 reais.
+// Se o cliente escolher whey hidrolisado, o preço é 200 reais.
+
 package exe
+
+func main() {
+
+}
