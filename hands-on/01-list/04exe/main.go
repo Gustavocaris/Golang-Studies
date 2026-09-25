a755081608a41a116d2dad18517911e2270060ab
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-09-25 18:24:18 -0300

    feat(hands-on): Initial structure of the exercise with the declared function

--- hands-on/01-list/04exe/main.go
+++ hands-on/01-list/04exe/main.go
@@ -1,10 +1,17 @@
 // faça um programa que vende potes de Whey.
-// Se o cliente escolher whey normal, o preço é 100 reais.
-// Se o cliente escolher whey isolado, o preço é 150 reais.
-// Se o cliente escolher whey hidrolisado, o preço é 200 reais.
+// Se o cliente escolher whey normal 1, o preço é 100 reais.
+// Se o cliente escolher whey isolado 2, o preço é 150 reais.
+// Se o cliente escolher whey hidrolisado 3, o preço é 200 reais.
 
-package exe
+package main
 
 func main() {
+	var option int
+	print("Qual Whey voce gostaria de escolher ? 1 -> Normal 2 -> Isolado 3 -> hidrolisado")
+
+	scanf("")
+
+	switch option {
+	}
 
 }
